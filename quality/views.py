a5d96f3d5bb22fc8b9aa904586197be7023099b2
cutty12 <cuttyswiss@protonmail.com>
2020-10-29 17:04:26 +0100

    Added API for weather quality and created view for data on home.html

--- quality/views.py
+++ quality/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here
 def home(request):
-	return render(request, "home.html", {})
+	import json
+	import requests
+
+	api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=75211&distance=5&API_KEY=7CEAEF41-A2C2-410A-8D31-4892D1D90F69")
+
+	try:
+		api = json.loads(api_request.content)
+	except Exception as e:
+		api = "Error..."
+
+	return render(request, 'home.html', {"api": api})
+
 
 def about(request):
 	return render(request, "about.html", {})
